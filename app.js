b82acbbd147a44e4daab833c6f47fc26bcd78e8d
DahLevi <dahlbergl@spu.edu>
2021-06-07 22:54:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -38,6 +38,17 @@ app.use('/api/books', bookRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+});
+
+//Fix CORS error for LocalHost Testing
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+// Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+// Pass to next layer of middleware
+  next();
 });
 
 // error handler
