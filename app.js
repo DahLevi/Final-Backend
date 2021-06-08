37286bee69d15debdc45e0300c9f6f69f515abf6
DahLevi <dahlbergl@spu.edu>
2021-06-08 11:19:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -36,11 +36,6 @@ app.use('/api/books', bookRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-// Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-// Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 // Pass to next layer of middleware
   next(createError(404));
 });
