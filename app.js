bb993eef7ae521a7aeb55d08bebd07949e2d1c5d
DahLevi <dahlbergl@spu.edu>
2021-06-07 22:50:08 -0700

    Cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 import cors from 'cors';
 
-app.use(cors());
+
 // for db
 var db = require('./database');
 
@@ -19,7 +19,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
